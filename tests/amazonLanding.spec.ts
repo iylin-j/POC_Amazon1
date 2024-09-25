ea3a43a547f6955858aa8b9c7a7072873354fb7c
iylin.j <jacob.ij@pg.com>
2024-09-25 19:01:00 +0530

    Updated the TC id

--- tests/amazonLanding.spec.ts
+++ tests/amazonLanding.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 import AmazonHomePage from '../pom/amazon.page';
 
-test('Validate if the user is able to land on to different tabs/categories', async ({ page }) => {
+test('[15448] Validate if the user is able to land on to different tabs/categories', async ({ page }) => {
   const amazonHomePage = new AmazonHomePage(page);
 
   await page.goto('https://www.amazon.in/');
@@ -9,12 +9,20 @@ test('Validate if the user is able to land on to different tabs/categories', asy
   await amazonHomePage.validateShoppingContainer();
 });
 
-test('Validate if user is not able to login with invalid credentials ', async ({ page }) => {
+test('[15449] Validate if user is not able to login with invalid credentials ', async ({ page }) => {
   const amazonHomePage = new AmazonHomePage(page);
 
   await page.goto('https://www.amazon.in/');
   await expect(page).toHaveURL("https://www.amazon.in/");
   await amazonHomePage.validateInvalidCredentials();
+});
+
+test('[15450] Validate if user is able to Create new Account', async ({ page }) => {
+  const amazonHomePage = new AmazonHomePage(page);
+
+  await page.goto('https://www.amazon.in/');
+  await expect(page).toHaveURL("https://www.amazon.in/");
+  await amazonHomePage.validateSignUpPage();
 });
 
 test('Validate user is able to search "iphone 16", and list contains the products in search bar', async ({ page }) => {
@@ -25,10 +33,4 @@ test('Validate user is able to search "iphone 16", and list contains the product
   await amazonHomePage.validateSearchBarDropDown();
 });
 
-test('Validate if user is able to Create new Account', async ({ page }) => {
-  const amazonHomePage = new AmazonHomePage(page);
 
-  await page.goto('https://www.amazon.in/');
-  await expect(page).toHaveURL("https://www.amazon.in/");
-  await amazonHomePage.validateSignUpPage();
-});
